3a7f1ae40bc6d1805e4841024ffd23b83d2d5651
ryanwardle <wardle2106@gmail.com>
2019-07-13 16:10:10 -0400

    editted form styles beginning work on gathering form info

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const onAddExercise = function onAddExercise(){
+
   // get element id
   const id = event.target.id;
-  
+
   // get button clicked on
   const clickedDay = document.getElementById(id);
 
@@ -15,4 +16,31 @@ const onAddExercise = function onAddExercise(){
 
   // insert new element into DOM
   clickedDay.insertAdjacentElement('beforebegin', newRow);
+}
+
+
+
+
+const onCreateWorkout = function onCreateWorkout (){
+
+  // GETTING FORM
+  const displaySection = document.getElementById('display-workout-section');
+  console.log(document.getElementById('workout-form'));
+  const form = document.getElementById('workout-form');
+
+// CYCLING THROUGH ELEMENTS IN FORM
+  for(let i = 0; i < form.length; i++){
+    let day = form[i].parentElement.previousElementSibling;
+    console.log(day);
+    if(form[i].type === 'checkbox' && form[i].checked === true){
+      let day = form[i].parentElement.previousElementSibling.innerText.toLowerCase();
+      // let workoutDisplayDay = document.getElementById(`${day}-workout`);
+      // console.log(workoutDisplayDay)
+
+      // CANNOT GET WORKOUT DAYS, I THINK BECAUSE IT IS NOT YET IN THE DOM
+      console.log(document.getElementById('monday-workout'))
+    }
+  }
+
+  displaySection.style.display = 'block';
 }
