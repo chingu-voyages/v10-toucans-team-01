7d93d89f39058db847bb872a1eabbd4dcb65971b
ryanwardle <wardle2106@gmail.com>
2019-07-22 21:44:03 -0400

    continued work on alert if form is invalid, clears alert if not closed once form is submitted as valid

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 // TO DO:
-// 6. DISPLAY MESSAGE TO COMPLETE FORM IF NOT VALID
 // 7. CLEAR SUBMISSION FORM WITH FORM.RESET() IF X OR CLOSE PRESSED?
 
 const onAddExercise = function onAddExercise(){
@@ -30,7 +29,8 @@ const disableButtons = function disableButtons(inputClass) {
 
   // TAKES OUT -NON TO GET "NAME" ATTRIBUTE OF DAY;
   const index = inputClass.indexOf('-non');
-  const nameAttrName = inputClass.slice(0, index) + inputClass.slice(index + 4);
+  const day = inputClass.slice(0, index);
+  const nameAttrName = day + inputClass.slice(index + 4);
 
   // GET CHECKBOX INPUT FOR SPECIFIC DAY
   const restDay = document.getElementsByName(nameAttrName);
@@ -39,6 +39,9 @@ const disableButtons = function disableButtons(inputClass) {
   for(const obj of inputs){
       obj.disabled = restDay[0].checked;
   }
+
+  const addExerciseButton = document.getElementById(`add-${day}-exercise`);
+  addExerciseButton.disabled = restDay[0].checked;
 }
 
 
@@ -59,7 +62,7 @@ const insertExercise = function insertExercise (key, value){
       let exerciseBox;
       let input = [...value]
 
-// TAKES CARE OF DAYS WITH ONLY ONE EXERCISE
+// TAKES CARE OF DAYS WITH ONLY ONE EXERCISE CREATES ARRAY INSTEAD OF STRING
       if(typeof(value) === 'string'){
         input = [value];
        }
@@ -75,7 +78,7 @@ const insertExercise = function insertExercise (key, value){
 
         const inputValue = document.createElement('p');
 
-
+// ADDS SETS, REPS AND POUNDS TO INPUTS, LEAVES EXERCISE AS IS
         if(action === 'sets' || action === 'reps'){
           inputValue.innerText = `${input[i]} ${action}`;
         }else if(action === 'weight'){
@@ -91,18 +94,24 @@ const insertExercise = function insertExercise (key, value){
 }
 
 
-const onCreateWorkout = function onCreateWorkout (event){
+const onCreateWorkout = function onCreateWorkout (){
 
   // GETTING FORM
   const displaySection = document.getElementById('display-workout-section');
   const form = document.getElementById('workout-form');
 
+
   if(form.checkValidity()){
+
+// IF ALERT IS SHOWING AND NOW FORM IS VALID HIDE ALERT
+    if(document.getElementById('incomplete-alert')){
+      document.getElementById('incomplete-alert').remove();
+    }
+
     const request = new XMLHttpRequest();
-
     request.open('POST', 'https://httpbin.org/post', /* async = */ false);
-    const data = new FormData(form);
 
+    const data = new FormData(form);
     request.send(data);
 
   // PARSES RESPONSE TO JSON AND THEN GETS FORM
@@ -111,20 +120,35 @@ const onCreateWorkout = function onCreateWorkout (event){
   // LOOPS THROUGH OBJECT AND CALLS INSERTEXERCISE TO INSERT INFO INTO DOM
     for(const key in createdWorkout){
       const value = createdWorkout[key];
+
+      // CALL INSERT EXERCISE FUNCTION ON KEY VALUE PAIRS
       insertExercise(key, value);
     }
 
     form.reset();
     displaySection.style.display = 'block';
 
-  } else{
-    // const incompleteModal = document.getElementById('incomplete-form-modal');
-    // incompleteModal.classList.add('show');
-    const incompleteAlert = document.getElementById('incomplete-form-alert');
-    incompleteAlert.classList.add('show');
-    console.log('tell person to fill out form')
+  } else if(!document.getElementById('incomplete-alert')){
 
+    // IF NO ALERT EXISTS IN DOM THEN CREATE ONE TO TELL USER TO FILL OUT ENTIRE FORM
+    const alertContainer = document.createElement('div');
+    alertContainer.classList.add('alert-container');
+    
+    const incompleteAlert = document.createElement('div');
+    incompleteAlert.setAttribute('id', 'incomplete-alert');
+
+    const closeButton = document.createElement('button');
+    const closeSpan = document.createElement('span');
+
+    incompleteAlert.classList.add('alert', 'alert-warning', 'alert-dismissable', 'fade', 'show');
+    incompleteAlert.innerText = 'Please complete the entire workout form before submission.';
+    closeButton.classList.add('close');
+    closeButton.setAttribute('data-dismiss', 'alert');
+    closeButton.setAttribute('type', 'button');
+    closeSpan.innerHTML = '&times;';
+    closeButton.appendChild(closeSpan);
+    incompleteAlert.appendChild(closeButton);
+    alertContainer.appendChild(incompleteAlert);
+    document.getElementById('form-modal').insertAdjacentElement('afterend', alertContainer);
   }
-
-
 }
